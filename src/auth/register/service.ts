67346a9e9f7f36bd26589b5186d65577b9a48b74
ndtien142 <ndtien142@gmail.com>
2025-06-24 22:26:21 +0700

    fix call register

--- src/auth/register/service.ts
+++ src/auth/register/service.ts
@@ -1,6 +1,6 @@
 import { API_CUSTOMER_REGISTER } from 'src/common/constant/api.constant';
-import axiosInstance from 'src/common/utils/axios';
+import { axiosInstance3 } from 'src/common/utils/axios';
 import { IResLogin } from '../login/interface';
 
 export const registerNewAccount = async (data: any) =>
-  axiosInstance.post<unknown, IResLogin>(API_CUSTOMER_REGISTER, data);
+  axiosInstance3.post<unknown, IResLogin>(API_CUSTOMER_REGISTER, data);
